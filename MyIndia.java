5a662e99bc9f46927025327fd531c175ef2ec6ae
G Ajeet <gajeet11000@gmail.com>
2019-12-30 21:48:17 +0530

    Update MyIndia.java

--- MyIndia.java
+++ MyIndia.java
@@ -1,11 +1,3 @@
-/*------------------------------------------------Instructions to run the Program-------------------------------------------------------
-1. Select and copy this whole code.
-2. Now you may goto any online Java complier OR just goto the link below:
-   "https://www.tutorialspoint.com/compile_java_online.php"
-3. Clear the default "Hello World" program there.
-4. Now paste this code there.
-5. Click on Execute and enjoy the Beauty of Programming!!
----------------------------------------------------------------------------------------------------------------------------------------*/
 /**
  * A character encoded java program to print the figure of India.
  * 
